3ca1ae3a68f184a7569689773ade65df8ec7f072
Kerriea-star <ojared511@gmail.com>
2022-07-12 17:41:14 +0300

    Iterating with for loop

---
+++ forLoop.js
@@ -0,0 +1,13 @@
+var ourAray = [];
+
+for (var i = 0; i < 5; i++){
+    ourAray.push(i);
+}
+
+console.log(ourAray);
+
+var myArray = [];
+ for (var a = 1; a < 6; a++){
+    myArray.push(a);
+ }
+ console.log(myArray);
