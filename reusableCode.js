9fdc3bd9564b00f3e643ec52ae0ffc12afa004c3
Kerriea-star <ojared511@gmail.com>
2022-07-12 08:32:45 +0300

    Assignment with a return value

--- reusableCode.js
+++ reusableCode.js
@@ -70,3 +70,19 @@ function addFive(){
 console.log(addFive());
 
 // Assignment with a return value
+var changed = 0;
+
+function change(num){
+    return (num + 5) / 3;
+}
+
+changed = change(10);
+console.log(changed);
+
+var processed = 0;
+
+function processArg(num){
+    return (num + 3) / 5;
+}
+processed = processArg(7);
+console.log(processed);
