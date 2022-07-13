19e5a79ffdb96cbd3ff253533f34e180314eba4f
Kerriea-star <ojared511@gmail.com>
2022-07-13 14:08:40 +0300

    importing a default export

--- import.js
+++ import.js
@@ -5,3 +5,8 @@ console.log(cap);
 
 // importing everything from another file using "*" as shown below
 import * as capitalizeStrings from "./export";
+
+// importing a default export
+import subtract from "math_functions";
+
+subtract(7, 4);
