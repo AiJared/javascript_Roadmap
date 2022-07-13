ef9b0b998343d9b28acb5b630afd68303e112993
Kerriea-star <ojared511@gmail.com>
2022-07-13 08:41:37 +0300

    Generate a random fraction using random method

---
+++ RandomFraction.js
@@ -0,0 +1,8 @@
+// we use random() to generate a random decimal number
+// This is a number that is between 0 and 1 but not 1 as shown in the example below
+function randomFraction() {
+
+    return Math.random();
+}
+
+console.log(randomFraction());
