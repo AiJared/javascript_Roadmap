7e5224eea87f2a2b811a7487d8d44beb25a3e8cc
Kerriea-star <ojared511@gmail.com>
2022-07-13 14:01:08 +0300

    More on export in js

--- export.js
+++ export.js
@@ -1,1 +1,8 @@
-export const capitalizeString = str => str.toUpperCase()
+const capitalizeString = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export { capitalizeString };
+
+export const foo = "bar";
+export const bar = "foo";
