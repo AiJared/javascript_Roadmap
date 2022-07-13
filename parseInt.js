788502430d66fc740dd81a4016594bde40fce9f3
Kerriea-star <ojared511@gmail.com>
2022-07-13 09:38:56 +0300

    parseInt with a radix

--- parseInt.js
+++ parseInt.js
@@ -4,3 +4,11 @@ function convertToInteger(str) {
 }
 
 console.log(convertToInteger("56"));
+
+// parseInt with a radix
+// we will convert a binary number into an integer with a base of 2 as shown below
+function convertToInteger(str) {
+    return parseInt(str, 2);
+}
+
+convertToInteger("10011");
