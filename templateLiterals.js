0dd992d3164de2efdb6e2c01be480cea07db9524
Kerriea-star <ojared511@gmail.com>
2022-07-13 13:21:12 +0300

    Function in object and constructor function

---
+++ templateLiterals.js
@@ -0,0 +1,4 @@
+// More on objects
+//first name is the key and the second name is the vale. The same applies to age and gender respecively
+const createPerson = (name, age, gender) => ({name, age, gender });
+console.log(createPerson("Zodiac Hasbro", 56, "male"));
