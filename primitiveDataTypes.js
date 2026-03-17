c65f58a77dc34711bb1525621dcc3fc1aa1fcb5e
AiJared <ojared511@gmail.com>
2026-03-17 07:54:34 +0300

    numbers and BigInt types

--- primitiveDataTypes.js
+++ primitiveDataTypes.js
@@ -62,3 +62,40 @@ let str3 = "New \nline";
 let str4 = "This contains a backslassh \\";
 console.log(str3);
 console.log(str4);
+
+// Numbers
+/*
+This is one of those areas where Javascript differs with most other languages.
+The numbers type represent well, mnumbers. Whether integers, decimals,
+hexadecimals, exponentials, octal and binary numbers.
+Most other languages store each number type differently, for example whole
+numbers as integers, decimals as floats and so forth.
+The example below demonstrates how Javascript does it.
+*/
+
+let intNr = 1;
+let decNr = 1.5;
+let expNr = 1.4e15;
+let octNr = 0o10 // decimal version would be 8
+let hexNr = 0x3E8 // decimal version would be 1000
+let biNr = 0b101 // decimal version would be 5
+
+/*
+All the above are of the number data type. However, you might need to represent
+an even bigger number. That's where the data type below comes in.
+*/
+
+// BigInt
+/*
+The limits of the number type are within 2^53 and -2^53. If you need to 
+store a bigger number or a much smaller number you would need the BigInt
+type. A BigInt type can be recognized by the postfix n. Below is an example
+*/
+
+let bigNr = 90071992547409920n;
+
+/*
+If we try to add a BigInt with a number for example, it will throw a
+TypeError. We cannot mix up a BigInt with a number when doing operations.
+BigIn can only work with another BigInt.
+*/
