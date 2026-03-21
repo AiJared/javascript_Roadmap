d443778a8c097fcc8c1b0cda1b3c238e6ca25755
AiJared <ojared511@gmail.com>
2026-03-21 14:10:22 +0300

    operators

---
+++ operators.js
@@ -0,0 +1,6 @@
+/*
+Operators come in handy when we want to work with variables. They help with
+different modifications including calculations and comparisons. They are
+called operators because we use them to perform operations on our variables.
+*/
+
