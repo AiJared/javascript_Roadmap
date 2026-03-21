f83b25fc9d111341f1d783b4105ea6e66ddecbb1
AiJared <ojared511@gmail.com>
2026-03-21 14:16:25 +0300

    addition operator

--- operators.js
+++ operators.js
@@ -4,3 +4,27 @@ different modifications including calculations and comparisons. They are
 called operators because we use them to perform operations on our variables.
 */
 
+// Arithmetic Operators
+/*
+These are used for mostly mathematical tasks.
+*/
+
+// Let's start with addition
+/*
+This is straightforwad with numbers. We use the + operator to add two 
+numbers together.
+*/
+
+let nr1 = 10;
+let nr2 = 20;
+let result1 = nr1 + nr2;
+
+/*
+However, the + operator can also be used to concatenate two strings together
+in Javascript, check out the example below. Note the added space after "Hello "
+to ensure space between it and the next string.
+*/
+
+let str1 = "Hello ";
+let str2 = "Jared";
+let result2 = str1 + str2;
