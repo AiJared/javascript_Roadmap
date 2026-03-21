9d62442156f08d22986bb051b052a1fc970f09f1
AiJared <ojared511@gmail.com>
2026-03-21 14:24:02 +0300

    practice exercise with addition operator in JS

---
+++ Exercises/additionOperator.js
@@ -0,0 +1,15 @@
+/* 
+Create a variable for your name, another one for your age, and another one for
+whether you can code JavaScript or not.
+Log to the console the following sentence, where name, age and true/false are
+variables:
+
+Hello, my name is Maaike, I am 29 years old and I can code JavaScript:
+true.
+*/
+
+let nme = "Jared";
+let age = 26;
+let code = true;
+
+console.log("Hello my name is " + nme + " I am " + age + " years old and I can code Javascript: " + code + ".");
