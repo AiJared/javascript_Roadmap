eb101265833f0abafa8eeeb73dc7146104b41c50
AiJared <ojared511@gmail.com>
2026-03-23 07:13:49 +0300

    exponentiation operation

--- moreArithmeticOperators.js
+++ moreArithmeticOperators.js
@@ -1,2 +1,12 @@
 // This is a continuation of Arithmetic Operators
 // Let's Continue With Exponentiation
+/*
+Exponentiation means raising a certain base number to the power of the exponent.
+A good example is x to the power of y. It means that we multiply x by itself
+y number of times. Let's take a look at the example below.
+*/
+
+let nr1 = 2;
+let nr2 = 3;
+result = nr1 ** nr2;
+console.log(result);
