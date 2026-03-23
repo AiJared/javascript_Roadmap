bd3842699678ca800d9d200b4d06def4d01dfebf
AiJared <ojared511@gmail.com>
2026-03-23 07:17:18 +0300

    fractional exponentiation

--- moreArithmeticOperators.js
+++ moreArithmeticOperators.js
@@ -10,3 +10,12 @@ let nr1 = 2;
 let nr2 = 3;
 result = nr1 ** nr2;
 console.log(result);
+
+/*
+Now we can also find the square root of a number using fractional
+exponentiation, that is raising the number to 0.5. Example;
+*/
+
+let nr3 = 16;
+let sqrtNr3 = nr3 ** 0.5;
+console.log(sqrtNr3);
