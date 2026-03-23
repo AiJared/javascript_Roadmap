9f259423b501c26180beed77352362ef57d4fbe7
AiJared <ojared511@gmail.com>
2026-03-23 07:44:28 +0300

    unary operation

---
+++ unaryOperation.js
@@ -0,0 +1,21 @@
+/*
+There are two more arithmetic operations and those are increment and 
+decrement. A term we use here is "operands". Operands are subject to the
+operator for example in x + y, x and y are operands. 
+Now here we only need one operand, hence the name "unary", so we will do
+x++ in place of x = x + 1 or x-- in place of x = x - 1. 
+Let's take a look at some examples below.
+*/
+
+let nr1 = 4;
+nr1++;
+console.log(nr1);
+
+let nr2 = 4;
+nr2--;
+console.log(nr2);
+
+/*
+As you can see in the logs, the output is 5 and 3 respectively.
+*/
+
