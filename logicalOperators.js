c84cfb69353ea7d00e87dd9e66d5efede0b09257
AiJared <ojared511@gmail.com>
2026-03-26 09:30:42 +0300

    logical and

--- logicalOperators.js
+++ logicalOperators.js
@@ -10,3 +10,14 @@ If you want to check if x is greater than y and y is greater than z then
 you would need to combine two expressions. You will use the && operator and
 it will only return true if both expressions are true.
 */
+
+let x = 1;
+let y = 2;
+let z = 3;
+
+console.log(x < y && y < z);
+
+/*
+It logs true because both expressions are true, x really is smaller than y
+and y is smaller than z.
+*/
