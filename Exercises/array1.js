76cfa1a2f3e469a76557068178386244ac994a72
AiJared <ojared511@gmail.com>
2026-03-29 11:03:39 +0300

    The first arrays exercise

---
+++ Exercises/array1.js
@@ -0,0 +1,12 @@
+/*
+1. Create an array to use as your shopping list with 3 items: "Milk," "Bread," and
+"Apples."
+2. Check your list length in the console.
+3. Update "Bread" to "Bananas."
+4. Output your entire list to the console.
+*/
+
+shList = ["Milk", "Bread", "Apples"];
+console.log("The length of the shopping list is: ", shList.length);
+shList[1] = "Bananas";
+console.log("The entire shopping list: ", shList);
