1b562b4a7e04ba92d59e524f9cc8b4effc115c01
AiJared <ojared511@gmail.com>
2026-04-04 09:13:32 +0300

    multidimensional arrays

--- multidimentional_arrays.js
+++ multidimentional_arrays.js
@@ -35,4 +35,28 @@ console.log(value2);
 /*
 As you can see the first one takes the second element of the first
 array and second takes the third element of the third array.
+
+And it doesn't stop there, it can go way deeper than that. Let's
+demonstrate this well by creating an array of our array of arrays.
+We are simply going to store this array three times in another
+array.
+*/
+
+arrOfArraysOfArrays = [arrOfArrays, arrOfArrays, arrOfArrays];
+console.log(arrOfArraysOfArrays);
+
+/* Let's try to get the middle element which is 5 and it is also
+located in the second array which is the middle array of the second
+array that is also in the middle.
+Confusing? Well we have three arrays each of which is a multidimensional
+array and they are all in one array, get it now?
+*/
+let middleValue = arrOfArraysOfArrays[1][1][1];
+console.log(middleValue);
+
+/*
+The first step is to get the second array of arrays which is index
+1, then we get the second array inside of it which is also in index
+1 then finally we get the it's second value which again is in index
+1.
 */
