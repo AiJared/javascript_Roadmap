20e6305b419d9f2e1b98c381d44428603560f017
AiJared <ojared511@gmail.com>
2026-04-13 13:03:58 +0300

    second objects exercise

--- Exercises/objectsExercise.js
+++ Exercises/objectsExercise.js
@@ -31,3 +31,13 @@ myCar[variable] = false;
 
 console.log("The make of the car is " + myCar.make + " and the model is " + myCar.model);
 console.log("Is the car for sale? " + myCar.forSale);
+
+// Exercise #2
+/*
+1. Create an object named people that contains an empty array that is called
+friends.
+2. Create three variables, each containing an object, that contain one of your
+friend's first names, last names, and an ID value.
+3. Add the three friends to the friend array.
+4. Output it to the console.
+*/
